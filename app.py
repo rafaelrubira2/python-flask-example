e1c6113872817e3e83cdf81421f38e5ef66ad73c
rafaelrubira2 <rafaelrubira2@gmail.com>
2022-10-27 22:19:31 -0300

    ajustes_py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ env_var = os.environ.get("APP_PORT", "3000")
 
 app = Flask(__name__)
 
+@app.route("/health")
+def hello_health():
+    return "ok", HTTP_200_OK
+
 @app.route("/")
 def hello_world():
     return "ola mundo"
